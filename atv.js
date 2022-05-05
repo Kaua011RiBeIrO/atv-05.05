919aeb32f1420eaf80ee36fc13303d8920238477
Kaua011RiBeIrO <102737682+Kaua011RiBeIrO@users.noreply.github.com>
2022-05-05 09:00:31 -0300

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,38 @@
+//Exercicios-Kauã Ribeiro De Oliveira
+//1-a
+
+console.log(10 + 20*30);
+
+//1-b
+
+console.log(4**2 / 30);
+
+//1-c
+
+console.log((9**4 + 2) * 6 - 1);
+
+//2-a
+
+console.log(10 % 3 * 10**1 + 1 - 10 * 4 / 2);
+
+const a=6
+const b=9
+const c=4
+
+//3
+
+console.log("Kauã")
+ 
+//4
+
+console.log((2*3)*(3*5))
+
+//5
+
+console.log( a + b + c)
+
+//6
+
+const salario = 1800
+
+console.log(salario*2)
